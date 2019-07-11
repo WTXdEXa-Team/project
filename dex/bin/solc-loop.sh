79b301e624a5d3b53152f560a706a4afd0ac9f17
WFL Group <52611911+WTXdEXa-Team@users.noreply.github.com>
2019-07-11 14:13:30 +0900

    Create solc-loop.sh

---
+++ dex/bin/solc-loop.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)/..
+SRC=contracts/
+
+while true; do
+    (
+        find $SRC -name '*.sol' # Solidity sources
+        # monitor this tool's dependencies to help its own development
+        ls bin/.solc-colors bin/solc.sh
+    ) | \
+    entr -d $(dirname $0)/solc.sh $(find $SRC -name '*.sol')
+done
