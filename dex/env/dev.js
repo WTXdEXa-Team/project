0712c0251e35e5d6efca40753f5bb0bdff95a593
WFL Group <52611911+WTXdEXa-Team@users.noreply.github.com>
2019-07-17 17:56:17 +0900

    Create dev.js

---
+++ dex/env/dev.js
@@ -0,0 +1,12 @@
+export const config = {
+  networkId: 1337,
+  chainUrl: 'http://localhost:8900',
+  whisperNodeUrl: 'ws://localhost:8901',
+  name: 'Local Dev Network'
+}
+//
+// export const config = {
+//   networkId: 4,
+//   chainUrl: 'http://localhost:8420',
+//   name: 'Local Rinkeby Test Network'
+// }
